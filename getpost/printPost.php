f7194a467f2602075a6374f0f43010d144294a30
a <a@a.com>
2016-04-12 10:30:41 +0100

    u

--- getpost/printPost.php
+++ getpost/printPost.php
@@ -5,3 +5,13 @@
  * Date: 12/04/2016
  * Time: 09:30
  */
+$title=$_POST["title"];
+$content=$_POST["content"];
+$category=$_POST["category"];
+ echo "<article>";
+echo "<h1>$title</h1>";
+echo "<p>$content</p>";
+echo "<strong>$category</strong>";
+
+
+   ?>
