d3ac89f2966b7e8a706b27d24b56efa7b18343d7
a <a@a.com>
2016-04-12 10:36:37 +0100

    u

--- getpost/printPost.php
+++ getpost/printPost.php
@@ -10,9 +10,9 @@ $title=$_POST["title"];
 $content=$_POST["content"];
 $category=$_POST["category"];
  echo "<article>";
-echo "<h1>$title</h1>";
-echo "<p>$content</p>";
+echo "<h1>$title</h1></br>";
+echo "<p>$content</p></br></br>";
 echo "<strong>$category</strong>";
-echo "hello";
 
-   ?>
+
+?>
