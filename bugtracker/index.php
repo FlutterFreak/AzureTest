c44097d02bbdd5524e0f41675a703a64e337b5c6
a <a@a.com>
2016-04-25 10:27:15 +0100

    u

---
+++ bugtracker/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+-->
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+         <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap/js/jquery-1.12.0.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+       <link href="style.css" rel="stylesheet" type="text/css"/>
+        <title>BugTracker</title>
+    </head>
+    <body>
+        
+        <div class="container border">
+
+            <header>
+                
+                
+            <div class="row border">
+                <div class="col-md-2">
+                    <img class ="image-logo" src="image/logo.png" alt="" />
+
+                    
+                </div>
+                
+                <div class="col-md-10">
+                        
+                        <h2>BugTracker</h2>
+                        <h3>Keeping Track all of the Peski little bugs</h3>
+                        
+                    </div>
+    
+            </div>
+            
+           
+                
+            </header>
+           
+            <div class="row">
+                <div class="col-md-2 min-height">
+                    
+                    <div class="navbar">
+                        <div class="row">
+                            <div class="col-md-12"><h5><a href="index.php">All Bug Items</a></h5></div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12"><h5><a href="showBugs.php">Android Bugs</a></h5></div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12"><h5><a href="showBugs.php">IOS Bugs</a></h5></div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12"><h5><a href="showBugs.php">Windows Bugs</a></h5></div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12"><h5><a href="addBugs.php">Insert Bug</a></h5></div>
+                            
+                        </div>
+                        
+        
+                        </div>
+                    </div>
+                    
+                
+                <div class="col-md-10 border min-height">
+                    <p>jdk djsij odjoj sokdpop sdoksok osdksk sdjds sjdsjc osjdoj sjdsj psokdpo osdjpj psokdop dkscos podkp jdk djsij odjoj sokdpop sdoksok osdksk sdjds sjdsjc osjdoj sjdsj psokdpo osdjpj psokdop dkscos podkpjdk djsij odjoj sokdpop sdoksok osdksk sdjds sjdsjc osjdoj sjdsj psokdpo osdjpj psokdop dkscos podkpjdk djsij odjoj sokdpop sdoksok osdksk sdjds sjdsjc osjdoj sjdsj psokdpo osdjpj psokdop dkscos podkpjdk djsij odjoj sokdpop sdoksok osdksk sdjds sjdsjc osjdoj sjdsj psokdpo osdjpj psokdop dkscos podkp</p>
+                </div>
+            </div>
+            
+            <div class="row">
+            
+                <div class="col-md-12 border footer">
+                    Designed by Shahid Baig
+                    
+                </div>
+                
+        </div>
+
+            </div>
+        
+               
+            
+        
+        
+        
+    </body>
+</html>
