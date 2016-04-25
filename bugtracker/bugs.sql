92db98f76ad21029a90a83029ed967d5de23b519
a <a@a.com>
2016-04-25 11:16:22 +0100

    u

---
+++ bugtracker/bugs.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `bugs` (
+  `bugID` int(4) NOT NULL AUTO_INCREMENT,
+  `bugName` varchar(50) NOT NULL,
+  `BugSummary` varchar(250) NOT NULL,
+  `BugCategory` varchar(50) NOT NULL,
+  PRIMARY KEY (`bugID`))
